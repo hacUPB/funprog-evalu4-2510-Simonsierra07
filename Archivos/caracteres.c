dc42f73b806719a09c10c5e0360cd78559ecf625
Simonsierra07 <simon.sierrav@upb.edu.co>
2025-05-08 14:43:29 +0000

    actividades de clase

--- Archivos/caracteres.c
+++ Archivos/caracteres.c
@@ -8,15 +8,19 @@ int main(int argc, char const *argv[])
     int letra = 1;
     char caracter;
     pfile = fopen("Texto2.txt", "a");
-    if (pfile) // NULL es falso, cualquier valor diferente es verdadero
+    if (!pfile) // NULL es falso, cualquier valor diferente es verdadero
     {
+        perror("No se pudo abrir el archivo.\n");
+        return 1;
+    }
         while(letra != '?')
         {
             caracter = fgetc(stdin);
-            
-           letra = fputc(caracter, pfile);
+            if (caracter != '?')
         }
-    }
+            letra = fputc(caracter, pfile);
+        
+    
 
 fclose (pfile);
     return 0;
